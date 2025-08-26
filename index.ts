3f0753c52f3ac57797e6e11477a52eae392a4090
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2025-08-26 03:24:23 +0000

    Deleted unnecessary ts files

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// /api/whoami route
+app.get("/api/whoami", (req: Request, res: Response) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(
+    `Request Header Parser Microservice is running on port ${PORT} 🚀`,
+  );
+});
